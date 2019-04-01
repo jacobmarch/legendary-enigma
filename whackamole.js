c1b1466f6c6c96abf2554b280c8f1b86fe1e90eb
marchywkaj21 <32488828+marchywkaj21@users.noreply.github.com>
2019-04-01 12:44:26 -0400

    started work on scoreAndTime

--- whackamole.js
+++ whackamole.js
@@ -1,28 +1,27 @@
 let x = 00;
 let y = 00;
 let score = 0;
-let gameStart = false;
+let goodMoleClicked;
+let badMoleClicked;
+let timer = 3;
 
 function setup() {
   createCanvas(750, 750);
-  drawGrid();
+  
+  setupGame();
+
 }
 
 function mousePressed(){
-if(gameStart === false){
-   gameStart = true; 
-}
- if(gameStart === true){
-   gameStart = false; 
-  }
+
 }
 
 function setupGame(){
 
-  if (gameStart === true){
+  
     drawGrid();
 scoreAndTime();
-}
+
 }
 
 function drawMoles(){
@@ -46,6 +45,24 @@ function drawGrid(){
 }
 
 function scoreAndTime(){
+  if(goodMoleClicked === true){
+    score = score + 1;
+     }
+  if(badMoleClicked === true){
+    score = score - 3
+  }
+  //adjusts score based on what type of mole was clicked on
+  if(score < 0){
+    score = 0;
+  }
+  //prevents negative scores
+  
+  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+    timer --;
+  }
+  if (timer == 0) {
+    text("GAME OVER", width/2, height*0.7);
+  }
 }
 
 function moleClicked(){
