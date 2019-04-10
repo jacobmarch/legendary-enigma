cb677bda177d3e956b5fcc7ab4d2ecba45f69c56
marchywkaj21 <32488828+marchywkaj21@users.noreply.github.com>
2019-04-10 12:23:33 -0400

    Second-to-last update

--- whackamole.js
+++ whackamole.js
@@ -3,15 +3,45 @@ let y = 00;
 let score = 0;
 let goodMoleClicked;
 let badMoleClicked;
-let timer = 5;
+let timer = 3;
+let grid = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+let boxClicked;
+let gMole;
+let bMole;
+let grandomx; 
+let grandomy; 
+let brandomx;
+let brandomy;
+let highScore = 0;
+let oldGMole;
+let oldBMole;
+
+function mousePressed(){
+  moleClicked();
+  scoreTrack();
+}
+
+function keyPressed(){
+  if(timer == 0){
+    if(keyCode == ENTER){
+      timer = 3;
+      score = 0;
+    }
+  }
+}
 
 function setup() {
   createCanvas(750, 750);
-  
-  
-  
-
+  grandomx = int(random(0,3));
+  grandomy = int(random(0,3));
+  brandomx = int(random(0,3));
+  brandomy = int(random(0,3));
 }
+
 function draw(){
  
   background(255);
@@ -22,30 +52,35 @@ if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60
     timer --;
   
   }
-  
-text("time: " + timer, width/2, height/2);
+ fill(0,0,0); 
+
   if (timer == 0) {
     background(255);
+    fill(0,0,0);
     text("GAME OVER", width/2, height*0.5);
-   
+    text("SCORE: " + score, width/2, height/2 + 32.5);
+    text("HIGH SCORE: " + highScore, width/2, height/2 + 75);
+    gameValue = false;
+    if(score > highScore){
+      highScore = score;
+    }
   }
+  if(timer != 0){
+  goodMole();
+  badMole();
+    text("score: "  + score, width/2, height/2 + 75);
+}
+  text("time: " + timer, width/2, height/2);
   
 }
-
 
 function setupGame(){
 
   
     
-scoreAndTime();
+
   drawGrid();
    
-}
-
-function drawMoles(){
-moleClicked();
-goodMole();
-badMole();
 }
 
 function drawGrid(){
@@ -62,27 +97,70 @@ function drawGrid(){
   line(0,height-1,width,height-1);
 }
 
-function scoreAndTime(){
-  if(goodMoleClicked === true){
+function scoreTrack(){
+  if(gMole == boxClicked){
     score = score + 1;
      }
-  if(badMoleClicked === true){
-    score = score - 3
+  if(bMole == boxClicked){
+    score = score - 3;
   }
   //adjusts score based on what type of mole was clicked on
   if(score < 0){
     score = 0;
   }
   //prevents negative scores
-  
+  if (score > highScore){
+    highscore = score;
+  }
  
 }
 
 function moleClicked(){
+  for(i = 0; i < 3; i++){
+    for(j = 0; j < 3; j++){
+      if(int(mouseX / 250) == i && int(mouseY / 250) == j){
+        boxClicked = grid[i][j];
+        console.log(boxClicked);
+      }
+    }
+  }
+  newGoodMole();
+  newBadMole();
+}
+
+function drawMoles(){
+  rect(0,0,0,0);
 }
 
 function goodMole(){
+  gMole = grid[grandomx][grandomy];
+  fill(0, 255, 0);
+  rect(grandomx * 250, grandomy *250, 250, 250);
+}
+
+function newGoodMole(){
+  if(gMole == boxClicked || bMole == boxClicked){
+    grandomx = int(random(0,3));
+    grandomy = int(random(0,3));
+    
+  }
 }
 
 function badMole(){
+  bMole = grid[brandomx][brandomy];
+  if(bMole == gMole){
+    brandomx = int(random(0,3));
+    brandomy = int(random(0,3));
+    bMole = grid[brandomx][brandomy];
+  }
+  fill(255,0,0);
+  rect(brandomx * 250, brandomy * 250, 250, 250);
+}
+
+function newBadMole(){
+  if(bMole == boxClicked || gMole == boxClicked){
+    brandomx = int(random(0,3));
+    brandomy = int(random(0,3));
+    
+  }
 }
